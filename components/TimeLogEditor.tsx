e7be30d06e9c6485520014327794bbdc8a6757c4
S, Saikumar <saikumarrajan@gmail.com>
2026-07-12 17:24:16 +0530

    fix: disable both body and html overflow, revert backdrop to bg-black/10

--- components/TimeLogEditor.tsx
+++ components/TimeLogEditor.tsx
@@ -57,11 +57,14 @@ export function TimeLogEditor({ cardId, onTimeLogCreated }: TimeLogEditorProps) 
   React.useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = 'hidden';
+      document.documentElement.style.overflow = 'hidden';
     } else {
       document.body.style.overflow = '';
+      document.documentElement.style.overflow = '';
     }
     return () => {
       document.body.style.overflow = '';
+      document.documentElement.style.overflow = '';
     };
   }, [isOpen]);
 
